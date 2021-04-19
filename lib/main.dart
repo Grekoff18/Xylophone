2e2017783683d0cee002532f0c088910962fad95
Grekoff18 <kolagrekalo@gmail.com>
2021-04-19 20:13:00 +0300

    feat: finish project

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,6 +24,22 @@ class Xylophone extends StatefulWidget {
 }
 
 class _XylophoneState extends State<Xylophone> {
+  final player = AudioCache();
+
+  void playSound(int soundNumber) {
+    player.play("note$soundNumber.wav");
+  }
+
+  Expanded getKey({Color colorName, int soundNumber}) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () => playSound(soundNumber),
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(colorName)
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +50,18 @@ class _XylophoneState extends State<Xylophone> {
         title: Text(widget.title),
       ),
       body: SafeArea(
-        child: Text("Hello world"),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            getKey(colorName: Colors.blue, soundNumber: 1),
+            getKey(colorName: Colors.red, soundNumber: 2),
+            getKey(colorName: Colors.yellow, soundNumber: 3),
+            getKey(colorName: Colors.green, soundNumber: 4),
+            getKey(colorName: Colors.orange, soundNumber: 5),
+            getKey(colorName: Colors.purple, soundNumber: 6),
+            getKey(colorName: Colors.indigo, soundNumber: 7),
+          ],
+        ),
       ),
     );
   }
